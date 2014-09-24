21c18550ecce3c04b4977bc3c55d70e6d958c750
Alexandre Bartel <alexandre.bartel@cased.de>
2014-09-24 11:41:00 +0200

    update script to select target projects

--- runtests.sh
+++ runtests.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+#
+# Usage: ./runtests.sh "commons-io" "commons-lang"
 
 TMP_DIR=`pwd`"/tmp/"
 SOOT_URL="https://ssebuild.cased.de/nightly/soot/lib/soot-trunk.jar"
 SOOT_JAR=${TMP_DIR}/"`basename $SOOT_URL`"
 ANDROID_JARS="/opt/android-platforms/"
 
-COMMONS_IO="commons-io"
 
 # setup 
 rm -f *.status
@@ -14,12 +15,15 @@ wget -P $TMP_DIR $SOOT_URL
 
 
 # run
-R=0
-cd ./${COMMONS_IO}
-./runSequential.sh  $SOOT_JAR $ANDROID_JARS
-R=$(($? + $R))
-cd ..
-echo $R > ./${COMMONS_IO}.status
+for i in "$@"; do
+  TARGET_TEST_DIR=$i
+  R=0
+  cd ./${TARGET_TEST_DIR}
+  ./runSequential.sh  $SOOT_JAR $ANDROID_JARS
+  R=$(($? + $R))
+  cd ..
+  echo $R > ./${TARGET_TEST_DIR}.status
+done
 
 # clean
 rm -rf $TMP_DIR
