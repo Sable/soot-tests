b86abcfe056a8bed6a95addf9f5ffe6b04da378d
Alexandre Bartel <alexandre.bartel@cased.de>
2014-10-31 13:19:23 +0100

    new test cases from lucene-solr

---
+++ lucene-solr/runSequential.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+TARGET="lucene-solr"
+SOOT_JAR=$1
+ANDROID_JARS=$2
+
+# setup
+
+rm -f "${TARGET}.coffi.out"
+rm -f "${TARGET}.asm.out"
+rm -f "${TARGET}.dexpler.out"
+
+# run
+R=0
+./runtest.sh "coffi" "$SOOT_JAR" "$ANDROID_JARS" &> "${TARGET}.coffi.out"
+R=$(($? + $R))
+./runtest.sh "asm" "$SOOT_JAR" "$ANDROID_JARS" &> "${TARGET}.asm.out"
+R=$(($? + $R))
+./runtest.sh "dexpler" "$SOOT_JAR" "$ANDROID_JARS" &> "${TARGET}.dexpler.out"
+R=$(($? + $R))
+
+exit $R
